b8422d702fdaf9e8514e4d75dc8f48b2fc6c62aa
kyle67 <daniels.kyle@gmail.com>
2020-02-17 11:30:36 -0500

    Created random dice generator

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-var randomNumber1 = Math.floor(Math.random()*6)
+var randomNumber1 = Math.floor(Math.random()*6) + 1;
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+var randomImageSource = "./images/" + randomDiceImage;
+
+var image1 = document.querySeclectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
